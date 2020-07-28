3408f9a81414dccb377ea25085e638dbbd7a975f
Alex Krantz <alex@krantz.dev>
2020-07-28 11:16:15 -0700

    Add sign in page

---
+++ signin/signin.js
@@ -0,0 +1,49 @@
+// Register event listeners on document load
+$(document).ready(() => firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION)
+    .then(function () {
+        // Redirect if already logged in
+        firebase.auth().onAuthStateChanged(user => {
+            if (user) {
+                // Notify user they are signed in
+                toastr.success(user.email, "Successfully signed in");
+
+                // Redirect after 2.5 seconds so they can see it
+                setTimeout(() => window.location.href = "/", 2500);
+
+            // Display sign in button if not logged in
+            } else toggleLoading(false);
+        });
+
+        // Setup provider
+        let provider = new firebase.auth.GoogleAuthProvider();
+
+        let googleButton = $("#googleBtn");
+
+        // Register sign-in callback
+        googleButton.on("click", () => {
+            toggleLoading(true);
+
+            // Start the sign in process
+            firebase.auth().signInWithPopup(provider)
+                .catch(err => {
+                    // Stop the loading
+                    toggleLoading(false);
+
+                    // Display error message
+                    if (err.code !== "auth/popup-closed-by-user") toastr.error(err.message, "Failed to login");
+                });
+            }
+        );
+    })
+    .catch(console.error)
+);
+
+/**
+ * Toggle the loading spinner
+ *
+ * @param on {boolean} whether the page is loading or not
+ */
+function toggleLoading(on) {
+    $("#loading-spinner")[on ? "show" : "hide"]();
+    $("#googleBtn")[on ? "hide" : "show"]();
+}
